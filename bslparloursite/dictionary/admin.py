059b82898902c9014018aef4058da1be296c153e
Nathanael Farley <nasfarley88@gmail.com>
2015-12-22 11:47:45 +0000

    Added things to admin interface.

--- bslparloursite/dictionary/admin.py
+++ bslparloursite/dictionary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import BSLEntry, EnglishEntry
+
+admin.site.register(BSLEntry)
+admin.site.register(EnglishEntry)
--- bslparloursite/videolibrary/admin.py
+++ bslparloursite/videolibrary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SourceVideo
+
+admin.site.register(SourceVideo)
