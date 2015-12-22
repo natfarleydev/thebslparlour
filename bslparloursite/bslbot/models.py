1c529b1a1b93faa9a2a7d3abbf6250e63905cfdc
Nathanael Farley <nasfarley88@gmail.com>
2015-12-22 12:42:59 +0000

    Finished transfering models.

---
+++ bslparloursite/bslbot/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from dictionary.models import BSLEntry
+
+# Create your models here.
+
+class Tweet(models.Model):
+    """Base class for a tweet."""
+    tweet = models.CharField(max_length=135)
+    suggested_tweet = models.CharField(max_length=136, blank=True)
+    last_tweeted = models.DateTimeField(blank=True)
+
+    def __unicode__(self):
+        return self.tweet
+    
+    class Meta:
+        abstract = True
+        
+
+class InfoTweet(Tweet):
+    """This class is purely for tweeting information.
+
+    This tweet is empty because it is identical to Tweet, but it has a
+    (potentially) different admin interface."""
+    pass
+
+
+class BSLDictionaryTweet(Tweet):
+    """BSLDictionary entry tweet model."""
+    bsl_entry = models.ForeignKey(BSLEntry)
--- bslparloursite/bslparloursite/settings.py
+++ bslparloursite/bslparloursite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'videolibrary',
     'dictionary',
+    'bslbot',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
