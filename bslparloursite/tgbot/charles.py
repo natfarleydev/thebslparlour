016de2b1bef9eb0f7215506c0db1fb497ed827b6
Nathanael Farley <nasfarley88@gmail.com>
2016-01-15 10:15:21 +0000

    Can now add videos to BSL Parlour through Charles.

--- bslparloursite/tgbot/charles.py
+++ bslparloursite/tgbot/charles.py
@@ -7,6 +7,7 @@ import shutil
 import myconf
 import os
 import hashlib
+import logging
 from moviepy.editor import VideoFileClip
 import datetime
 
@@ -18,27 +19,30 @@ from sizefield.utils import parse_size
 from tgbot.models import RequestedSign
 from dictionary.models import BSLEntry
 from videolibrary.models import SourceVideo
+from bslbot.models import BSLDictionaryTweet
+
+logging.basicConfig(level=logging.DEBUG)
 
 @asyncio.coroutine
 def upload_to_dropbox(file_object, file_name, directory="/bslparlour/videos/tg_uploads/"):
     """A simple function to upload a thing to dropbox."""
     dbox_client = dropbox.client.DropboxClient(myconf.dbox_master_key)
-    print("Uploading to dropbox...")
+    logging.info("Uploading to dropbox...")
     retval = dbox_client.put_file(
         os.path.join(directory, file_name), file_object)
-    print("Uploaded to dropbox...")
+    logging.info("Uploaded to dropbox.")
     return retval
     
 @asyncio.coroutine
 def upload_to_vimeo(file_name):
-    print("Uploading to vimeo...")
+    logging.info("Uploading to vimeo...")
     v = vimeo.VimeoClient(
         token=myconf.vimeo_access_token,
         key=myconf.vimeo_client_identifier,
         secret=myconf.vimeo_client_secret
         )
+    logging.info("Uploaded to vimeo.")
     return v.upload(file_name)
-    print("Uploaded to vimeo.")
     
 
 @asyncio.coroutine
@@ -80,9 +84,122 @@ class Charles(telepot.helper.ChatHandler):
         return retval
     
     @asyncio.coroutine
+    def format_gloss(self, text):
+        """Attempts to reformat text into a gloss format.
+
+        (e.g. don't know -> DON'T-KNOW).
+
+        """
+
+        gloss = text.upper().replace(" ", "-")
+        logging.debug("Gloss formatted from "+text+" to "+gloss+".")
+        return gloss
+        
+
+    @asyncio.coroutine
+    def ask_for_gloss(self, should_confirm=True):
+        """Asks for a gloss from a user.
+
+        Attempts to format input from the user (e.g. don't know ->
+        DON'T-KNOW). By default, confirms input from user.
+
+        """
+
+        logging.debug("Entering loop in ask_for_gloss.")
+        while True:
+            gloss_msg = yield from self.request_info(
+                "Please enter a gloss for this sign.")
+            gloss = yield from self.format_gloss(gloss_msg['text'])
+
+            if should_confirm:
+                reply_keyboard = {"keyboard": [["Yes", "No"]]}
+                confirm_gloss_msg = yield from self.request_info(gloss+"\nIs this alright?",
+                    reply_markup=reply_keyboard)
+
+            else:
+                # If no need to confirm, break immediately after defining.
+                break
+
+            if confirm_gloss_msg["text"] == "Yes":
+                yield from self.sender.sendMessage("Gloss "+gloss+" accepted.", reply_markup={"hide_keyboard": True})
+                break
+        
+        return gloss
+        
+    @asyncio.coroutine
+    def send_bsl_entry(self, bsl_entry):
+        """Formats a given BSLEntry and then sends it to the user.
+
+        TODO get this function to send a video or animated gif of sign
+        (probably animated gif since this plays better and probably
+        has better compression to the end user by default).
+
+        """
+        # TODO get this to send a video
+        ret_str = bsl_entry.gloss+" "+str(bsl_entry.gloss_index)
+        return ret_str
+
+    @asyncio.coroutine
+    def create_or_append_to_bsl_entry(self, source_video, gloss=None, bsl_entry=None):
+        """Creates or appends the given source video to a BSLEntry.
+        
+        If gloss is None, this function fetches the gloss from the
+        user, otherwise it uses the gloss provided.
+
+        If bsl_entry is None, this function attempts to make smart
+        choices about whether to add to an existing entry or create a
+        new one (mostly by asking the user). If there are no matching
+        BSLentrys, it creates a new one immediately.
+
+        """
+
+        if gloss == None:
+            gloss = yield from self.ask_for_gloss()
+        elif isinstance(gloss, str):
+            gloss = yield from self.format_gloss(gloss)
+        else:
+            assert False, "gloss keyword argument must be str or None."
+        
+        # if bsl_entry is yet to be defined, the program makes an
+        # educated guess by consulting the user or creating an entry.
+        if bsl_entry == None:
+            bsl_entries_with_gloss = BSLEntry.objects.filter(gloss=gloss)
+            if len(bsl_entries_with_gloss) == 0:
+                bsl_entry = BSLEntry.objects.create(
+                    gloss=gloss,
+                    gloss_index=1
+                )
+            else:
+                for i in bsl_entries_with_gloss:
+                    self.send_bsl_entry(i)
+                reply_keyboard = [[i.gloss+" "+str(i.gloss_index)] for i in bsl_entries_with_gloss]
+                reply_keyboard.append(["Add new"])
+                bsl_entry_to_add_to = self.request_info(
+                    "Which BSLEntry should this be added to (if any)?",
+                    reply_markup=reply_keyboard)
+                if bsl_entry_to_add_to == "Add new":
+                    next_gloss_index = max([x.gloss_index for x in bsl_entries_with_gloss])+1
+                    bsl_entry = BSLEntry.objects.create(
+                        gloss=gloss,
+                        gloss_index=next_gloss_index
+                    )
+
+        bsl_entry.source_videos.add(source_video)
+        return bsl_entry
+
+    @asyncio.coroutine
     def create_source_video_from_msg(self, video_json):
- 
-        prefix =   datetime.datetime.strftime(
+        """Creates source video given an uploaded video from Telegram.
+
+        Returns SourceVideo.
+
+        This function automatically uploads the telegram video to
+        Dropbox and Vimeo, calculates the hash of the file and
+        generally fills in all details needed for a SourceVideo entry.
+        
+        """
+
+        prefix = datetime.datetime.strftime(
             datetime.datetime.now(),
             "%Y%m%d%H%M%S",
         )
@@ -92,19 +209,17 @@ class Charles(telepot.helper.ChatHandler):
             except KeyError:
                 yield from self.sender.sendMessage("Sorry, I must recieve a file.")
                 return
-            
+
             yield from self.sender.sendMessage("Processing video...")
 
             f.seek(0)
             dropbox_json = yield from upload_to_dropbox(f.file, f.name.split("/")[-1])
 
             vimeo_uri = yield from upload_to_vimeo(f.name)
-            
-            # TODO add logger stuffs for return things
-        
+
             f.seek(0)
             file_hash = yield from sha224_of_file(f)
-                
+
             source_video = SourceVideo.objects.create(
                 sha224=file_hash.hexdigest(),
                 filename=f.name.split("/")[-1],
@@ -116,83 +231,63 @@ class Charles(telepot.helper.ChatHandler):
 
         yield from self.sender.sendMessage(
             "Added to Dropbox, Vimeo, and SourceVideo table")
-        
+
         return source_video
-
-    @asyncio.coroutine
-    def send_bsl_entry(self, bsl_entry):
-        assert False, "Function not complete!"
-
-    @asyncio.coroutine
-    def get_single_bsl_entry_if_any(self, msg):
-        """If an entry is present, return it.
-
-           if there are multiple entries, ask the user which to return
-
-        """
-        assert False, "Make sure that the user wants to use one of the signs before this function is called."
-        try:
-            entry = BSLEntry.objects.get(gloss=msg['text'].upper())
-        except MultipleObjectsReturned as e:
-            entries = BSLEntry.objects.filter(gloss=msg['text'].upper())
-            yield from self.sender.sendMessage("Multiple possible duplicates found.")
-            for tmp_entry in entries:
-                yield from self.send_bsl_entry(tmp_entry)
-            
-            # make a keyboard of all the duplicates and then ask which
-            # of them it should be added to, if any.`
-            
-            reply_keyboard = [[x.gloss+" "+str(x.gloss_index)] for x in entries]
-            add_new_sign_text = "Add new sign"
-            reply_keyboard += [add_new_sign_text]
-            chosen_to_add = yield from self.request_info(
-                "Which would you like to add it to?",
-                reply_markup=reply_keyboard)
-            if chosen_to_add == add_new_sign_text:
-                return
-            elif chosen_to_add in [x[0] for x in reply_keyboard]:
-                return bsl
-                    
-            # TODO cancel the keyboard
-            yield from self.sender.sendMessage("Added to "+chosen_to_add+".")
-            # TODO get the bsl entry from the keyboard reply
-
-        assert False, "Function not complete!"
         
-        return entry
+        
+    @asyncio.coroutine
+    def create_bsl_dictionary_tweet(self, bsl_entry):
+        """Creates an automatic entry in the BSLDictionaryTweet table."""
+
+        logging.debug("Entering tweet into database.")
+        tweet = BSLDictionaryTweet.objects.create(
+            tweet = "This sign could be glossed "+bsl_entry.gloss+".",
+            suggested_tweet = "This sign could be glossed "+bsl_entry.gloss+".",
+            bsl_entry=bsl_entry,
+            last_tweeted=datetime.datetime(1970,1,1),
+        )
+        return tweet
+
         
     @asyncio.coroutine
     def upload_sign(self, msg):
         video_json = yield from self.request_info("Send your video!")
         source_video = yield from self.create_source_video_from_msg(video_json)
-        gloss = yield from self.request_info("What's the gloss for this sign?")
-        gloss = gloss.upper()
-        try:
-            gloss_index = max(
-                [x.gloss_index for x in BSLEntry.objects.filter(gloss=gloss)]
-            ) + 1 
-        except ValueError:
-            gloss_index = 1
-
-        if gloss_index != 1:
-            yield from self.sender.sendMessage("Multiple signs for this gloss detected. You may need to merge some later.")
-            for tmp_bsl_entry in BSLEntry.objects.filter(gloss=gloss):
-                with tempfile.NamedTemporaryFile() as f:
-                    tmp_source_video = tmp_bsl_entry.source_videos[-1]
-                    dbox_file, metadata = dbox_client.get_file_and_metadata(
-                        os.path.join(
-                            tmp_source_video.dropbox_directory, 
-                            tmp_source_video.filename))
-                    f.write(dbox_file.read())
-                    f.seek(0)
-                    yield from self.sender.sendVideo(f)
-                    
-        bsl_entry = BSLEntry.objects.create(
-            gloss=gloss,
-            gloss_index=gloss_index,
-        )
-        bsl_entry.source_videos.add(source_video)
         
+        # create_or_append_to_bsl_entry asks for a gloss if none is provided
+        bsl_entry = yield from self.create_or_append_to_bsl_entry(source_video)
+        tweet_entry = yield from self.create_bsl_dictionary_tweet(bsl_entry)
+
+        yield from self.sender.sendMessage("Tweet, BSLEntry, SourceVideo entered! Happy learning!")
+
+#         gloss = yield from self.request_info("What's the gloss for this sign?")
+#         gloss = gloss.upper()
+#         try:
+#             gloss_index = max(
+#                 [x.gloss_index for x in BSLEntry.objects.filter(gloss=gloss)]
+#             ) + 1 
+#         except ValueError:
+#             gloss_index = 1
+# 
+#         if gloss_index != 1:
+#             yield from self.sender.sendMessage("Multiple signs for this gloss detected. You may need to merge some later.")
+#             for tmp_bsl_entry in BSLEntry.objects.filter(gloss=gloss):
+#                 with tempfile.NamedTemporaryFile() as f:
+#                     tmp_source_video = tmp_bsl_entry.source_videos[-1]
+#                     dbox_file, metadata = dbox_client.get_file_and_metadata(
+#                         os.path.join(
+#                             tmp_source_video.dropbox_directory, 
+#                             tmp_source_video.filename))
+#                     f.write(dbox_file.read())
+#                     f.seek(0)
+#                     yield from self.sender.sendVideo(f)
+#                     
+#         bsl_entry = BSLEntry.objects.create(
+#             gloss=gloss,
+#             gloss_index=gloss_index,
+#         )
+#         bsl_entry.source_videos.add(source_video)
+#         
         # tweet_entry =           # blah blah
 
 
@@ -246,7 +341,9 @@ class Charles(telepot.helper.ChatHandler):
 
     @asyncio.coroutine
     def request_info(self, prompt, timeout=30, **kwargs):
-        yield from self.sender.sendMessage(prompt, kwargs)
+        logging.debug("kwargs for request_info: "+str(kwargs))
+        yield from self.sender.sendMessage(prompt, **kwargs)
+        logging.info("Message sent: "+prompt)
         l = self._bot.create_listener()
         l.set_options(timeout=timeout)
         l.capture(chat__id=self.chat_id)
